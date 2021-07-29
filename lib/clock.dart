456b741ea41590ec022ac08f44b77ae2dfbdc68f
Steve <steve.tyrell@gmail.com>
2021-07-30 00:04:23 +0100

    version 0.1

--- lib/clock.dart
+++ lib/clock.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
 import 'package:timer_builder/timer_builder.dart';
+import 'dart:async';
 
 class Clock extends StatefulWidget {
   @override
@@ -26,37 +27,96 @@ class _ClockState extends State<Clock> {
         // height: 400,
         child: Column(
           children: [
-            Container(
-              height: 50,
+            Flexible(
+              flex: 1,
+              child: Container(
+                  // color: Colors.blue,
+                  // height: 50,
+                  ),
             ),
-            Expanded(
-              child: Text(
-                "${getSystemTime()}",
-                style: TextStyle(
-                  decoration: TextDecoration.none,
-                  fontFamily: 'GamePlay',
-                  color: Colors.red,
-                  fontSize: 400,
-                  fontWeight: FontWeight.normal,
+            Flexible(
+              flex: 6,
+              child: Container(
+                child: FittedBox(
+                  fit: BoxFit.fitHeight,
+                  child: Text(
+                    "${getSystemTime()}",
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontFamily: 'GamePlay',
+                      color: Colors.red.shade900,
+                      fontSize: 400,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
                 ),
               ),
             ),
-            Container(
-              height: 10,
-            ),
-            Expanded(
-              child: Text(
-                "${getSystemSeconds()}",
-                style: TextStyle(
-                    decoration: TextDecoration.none,
-                    fontFamily: 'GamePlay',
-                    color: Colors.red,
-                    fontSize: 300,
-                    fontWeight: FontWeight.normal),
+            Flexible(
+              flex: 1,
+              child: Container(
+                // color: Colors.blue,
+                height: 10,
               ),
             ),
-            Container(
-              height: 50,
+            Flexible(
+              flex: 4,
+              child: Row(
+                // crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Flexible(
+                    flex: 2,
+                    child: Container(
+                      // color: Colors.white,
+                      // width: 400,
+                      // height: 50,
+                      alignment: Alignment.bottomCenter,
+                      child: FittedBox(
+                        fit: BoxFit.fitHeight,
+                        alignment: AlignmentDirectional.center,
+                        child: Text(
+                          "version 0.1",
+                          textAlign: TextAlign.end,
+                          style: TextStyle(
+                              decoration: TextDecoration.none,
+                              fontFamily: 'GamePlay',
+                              color: Colors.red.shade900,
+                              fontSize: 30,
+                              fontWeight: FontWeight.normal),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 3,
+                    child: Container(
+                      // color: Colors.teal,
+                      alignment: Alignment.center,
+                      child: FittedBox(
+                        fit: BoxFit.fitHeight,
+                        child: Text(
+                          "${getSystemSeconds()}",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              decoration: TextDecoration.none,
+                              fontFamily: 'GamePlay',
+                              color: Colors.red.shade900,
+                              fontSize: 300,
+                              fontWeight: FontWeight.normal),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Flexible(flex: 2, child: Container())
+                ],
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Container(
+                  // color: Colors.blue,
+                  // height: 50,
+                  ),
             ),
           ],
         ),
