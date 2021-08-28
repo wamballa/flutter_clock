1ee48282645db01a26e9fbad686b0ed6b68a9c44
Steve <steve.tyrell@gmail.com>
2021-08-28 19:42:38 +0100

    version 0.4

---
+++ lib/animation.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Animation_Page extends StatelessWidget {
+  const Animation_Page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print('ANIMATION');
+    return Container(
+      color: Colors.pink,
+    );
+  }
+}
--- lib/clock.dart
+++ lib/clock.dart
@@ -1,8 +1,10 @@
+/// Version 0.4 - hourly gif added
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
-import 'package:timer_builder/timer_builder.dart';
 import 'dart:async';
+import 'dart:math';
 
 class Clock extends StatefulWidget {
   @override
@@ -12,125 +14,180 @@ class Clock extends StatefulWidget {
 class _ClockState extends State<Clock> {
   @override
   void initState() {
+    startTimer();
     super.initState();
   }
 
-  final String currentTime = getSystemTime();
+  Color textColor = Colors.red.shade900;
+  bool isShowingAnimation = false;
+  bool isShowingClock = true;
+  String imageName = '';
+
+  void startTimer() {
+    imageName = getRandomImage();
+    const oneSec = const Duration(seconds: 1);
+    Timer.periodic(
+      oneSec,
+      (Timer timer) => setState(
+        () {
+          DateTime now = DateTime.now();
+          if (isShowingClock) {
+            // if (now.second % 5 == 0) {
+            if (now.minute == 0) {
+              isShowingClock = !isShowingClock;
+              imageName = getRandomImage();
+            }
+          }
+          if (!isShowingClock) {
+            if (now.second == 0) {
+              isShowingClock = true;
+            }
+          }
+        },
+      ),
+    );
+  }
+
+  String getRandomImage() {
+    List<String> imageFiles = [
+      'images/0.gif',
+      'images/1.gif',
+      'images/2.gif',
+      'images/3.gif',
+      'images/4.gif',
+      'images/5.gif',
+      'images/6.gif',
+      'images/7.gif',
+      'images/8.gif',
+      'images/9.gif',
+      'images/10.gif',
+      'images/11.gif',
+      'images/12.gif',
+      'images/13.gif',
+      'images/14.gif',
+      'images/15.gif',
+      'images/16.gif',
+      'images/17.gif',
+      'images/18.gif',
+      'images/19.gif',
+      'images/20.gif',
+    ];
+    int r = Random().nextInt(imageFiles.length - 1);
+    return imageFiles[r];
+  }
 
   @override
   Widget build(BuildContext context) {
-    return TimerBuilder.periodic(Duration(seconds: 1), builder: (context) {
-      return Container(
-        alignment: Alignment.center,
-        color: Colors.black,
-        // width: 800,
-        // height: 400,
-        child: Column(
-          children: [
-            Flexible(
-              flex: 1,
-              child: Container(
-                  // color: Colors.blue,
-                  // height: 50,
-                  ),
-            ),
-            Flexible(
-              flex: 6,
-              child: Container(
-                child: FittedBox(
-                  fit: BoxFit.fitHeight,
-                  child: Text(
-                    "${getSystemTime()}",
-                    style: TextStyle(
-                      decoration: TextDecoration.none,
-                      fontFamily: 'GamePlay',
-                      color: Colors.red.shade900,
-                      fontSize: 400,
-                      fontWeight: FontWeight.normal,
-                    ),
+    if (isShowingClock) {
+      return clockStuff();
+    } else
+      return aimationStuff();
+  }
+
+  String getSystemTime() {
+    var now = new DateTime.now();
+    return new DateFormat("HH:mm").format(now);
+  }
+
+  String getSystemSeconds() {
+    DateTime now = DateTime.now();
+    return new DateFormat("ss").format(now);
+  }
+
+  Container aimationStuff() {
+    return Container(
+      color: Colors.black,
+      padding: EdgeInsets.all(50),
+      child: FittedBox(
+        child: Image.asset(imageName),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Container clockStuff() {
+    return Container(
+      color: Colors.black,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Flexible(
+            flex: 6,
+            child: Container(
+              child: FittedBox(
+                fit: BoxFit.fitHeight,
+                child: Text(
+                  "${getSystemTime()}",
+                  style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontFamily: 'GamePlay',
+                    color: textColor,
+                    fontSize: 400,
+                    fontWeight: FontWeight.normal,
                   ),
                 ),
               ),
             ),
-            Flexible(
-              flex: 1,
-              child: Container(
-                // color: Colors.blue,
-                height: 10,
-              ),
-            ),
-            Flexible(
-              flex: 4,
-              child: Row(
-                // crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Flexible(
-                    flex: 2,
-                    child: Container(
-                      // color: Colors.white,
-                      // width: 400,
-                      // height: 50,
-                      alignment: Alignment.bottomCenter,
-                      child: FittedBox(
-                        fit: BoxFit.fitHeight,
-                        alignment: AlignmentDirectional.center,
+          ),
+          // Spacer(flex: 1),
+          Flexible(
+            flex: 4,
+            child: Row(
+              // crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Flexible(
+                  flex: 2,
+                  child: Container(
+                    alignment: Alignment.bottomCenter,
+                    child: FittedBox(
+                      fit: BoxFit.fitHeight,
+                      alignment: AlignmentDirectional.center,
+                      child: Padding(
+                        padding: EdgeInsets.all(7),
                         child: Text(
-                          "version 0.1",
+                          "version 0.4",
                           textAlign: TextAlign.end,
                           style: TextStyle(
                               decoration: TextDecoration.none,
                               fontFamily: 'GamePlay',
-                              color: Colors.red.shade900,
-                              fontSize: 30,
+                              color: textColor,
+                              fontSize: 10,
                               fontWeight: FontWeight.normal),
                         ),
                       ),
                     ),
                   ),
-                  Flexible(
-                    flex: 3,
-                    child: Container(
-                      // color: Colors.teal,
-                      alignment: Alignment.center,
-                      child: FittedBox(
-                        fit: BoxFit.fitHeight,
-                        child: Text(
-                          "${getSystemSeconds()}",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              decoration: TextDecoration.none,
-                              fontFamily: 'GamePlay',
-                              color: Colors.red.shade900,
-                              fontSize: 300,
-                              fontWeight: FontWeight.normal),
-                        ),
+                ),
+                Flexible(
+                  flex: 3,
+                  child: Container(
+                    // color: Colors.teal,
+                    alignment: Alignment.center,
+                    child: FittedBox(
+                      fit: BoxFit.fitHeight,
+                      child: Text(
+                        "${getSystemSeconds()}",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontFamily: 'GamePlay',
+                            color: textColor,
+                            fontSize: 300,
+                            fontWeight: FontWeight.normal),
                       ),
                     ),
                   ),
-                  Flexible(flex: 2, child: Container())
-                ],
-              ),
+                ),
+                Flexible(
+                  flex: 2,
+                  child: Container(),
+                ),
+              ],
             ),
-            Flexible(
-              flex: 1,
-              child: Container(
-                  // color: Colors.blue,
-                  // height: 50,
-                  ),
-            ),
-          ],
-        ),
-      );
-    });
+          ),
+        ],
+      ),
+    );
   }
-}
-
-String getSystemTime() {
-  var now = new DateTime.now();
-  return new DateFormat("HH:mm").format(now);
-}
-
-String getSystemSeconds() {
-  var now = new DateTime.now();
-  return new DateFormat("ss").format(now);
 }
